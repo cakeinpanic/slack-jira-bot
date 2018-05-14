a51b49676c1271dd178a571c72f43042163f2e4e
cakeinpanic <cakeinpanic@gmail.com>
2018-05-14 13:10:49 +0300

    update jira API

--- _config.js
+++ _config.js
@@ -4,11 +4,12 @@ module.exports = {
         botName: 'myBotName' // your bot display name
     },
     jira: {
-        host: 'jira.mycompany.com',
+        host: 'https://jira.mycompany.com',
         port: '80',
         user: '***',
         password: '***',
         projects: ['IBULF'], // in MPR-554 'MPR' would be a project name
-        useThread: true // publish issue info into thread or directly in channel
+        useThread: true, // publish issue info into thread or directly in channel,
+        issueLink: 'https://jira.mycompany.com/browse/' // optional, otherwise link to issue would be combined like https://{host}/browse/
     }
 };
